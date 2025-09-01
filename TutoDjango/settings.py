4c656423acf916ca1daa29cfda8f48482a78acee
Hun Valentin <o22306966@or-iut-i207-f27.iut45.univ-orleans.fr>
2025-09-01 16:56:46 +0200

    configuration des settings TutoDjango

--- TutoDjango/settings.py
+++ TutoDjango/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'monApp',
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -116,10 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (BASE_DIR/'static/',)
+STATICFILES_DIRS = (BASE_DIR / 'static/',)
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
