957341552acfc54568c7f3a133b415d5b9318171
Hun Valentin <valentin.hun@etu.univ-orleans.fr>
2025-09-08 15:57:38 +0200

    deuxième migration

--- monApp/models.py
+++ monApp/models.py
@@ -18,3 +18,10 @@ class Produit(models.Model):
 
     def __str__(self):
         return self.intituleProd
+    
+class Rayon(models.Model):
+    idRayon = models.AutoField(primary_key=True)
+    nomRayon = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nomRayon
