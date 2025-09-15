8c883ffb8077e3f55faeb007a8081b49dc369a25
Hun Valentin <valentin.hun@etu.univ-orleans.fr>
2025-09-15 17:00:14 +0200

    nouveau modèle

--- monApp/admin.py
+++ monApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Produit, Categorie, Statut, Rayon, Contenir
 
 # Register your models here.
+
+class ProduitAdmin(admin.ModelAdmin):
+    list_display = ('intituleProd', 'prixUnitaireProd')
+
+admin.site.register(Produit, ProduitAdmin)
+admin.site.register(Categorie)
+admin.site.register(Statut)
+admin.site.register(Rayon)
+admin.site.register(Contenir)
--- monApp/models.py
+++ monApp/models.py
@@ -1,47 +1,50 @@
+from datetime import date
 from django.db import models
-from django.utils import timezone
 
 # Create your models here.
-
 class Categorie(models.Model):
     idCat = models.AutoField(primary_key=True)
     nomCat = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nomCat
-    
+
 class Statut(models.Model):
-    idStatut = models.AutoField(primary_key=True)
-    libelle = models.CharField(max_length=100)
-    
+    idStatus = models.AutoField(primary_key=True)
+    libelleStatus = models.CharField(max_length=100)
+          
     def __str__(self):
-        return self.libelle
+        return self.libelleStatus
+    
 
 class Produit(models.Model):
     refProd = models.AutoField(primary_key=True)
     intituleProd = models.CharField(max_length=200)
     prixUnitaireProd = models.DecimalField(max_digits=10, decimal_places=2)
-    # Relation CIF : chaque produit appartient à 1 catégorie (0,N côté catégorie → 1,1 côté produit)
-    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, related_name="produits",null=True, blank=True)
+    dateFabProd = models.DateField(default=date.today)
 
-    date_fab = models.DateField(default=timezone.now)
-    statut = models.ForeignKey(Statut, on_delete=models.CASCADE, related_name="produits", null=True, blank=True)
+    # Relation CIF : chaque produit appartient à 1 catégorie (0,N côté catégorie → 1,1 côté produit)
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, related_name="produits_categorie", null=True, blank=True)
+    status = models.ForeignKey(Statut, on_delete=models.CASCADE, related_name="produits_status",null=True, blank=True)
 
     def __str__(self):
-        return "(" + str(self.refProd) + "," + self.intituleProd + "," + str(self.prixUnitaireProd) + ")"
-    
+        return self.intituleProd
+
 class Rayon(models.Model):
     idRayon = models.AutoField(primary_key=True)
     nomRayon = models.CharField(max_length=100)
 
     def __str__(self):
-        return "(" +  str(self.idRayon) + "," + self.nomRayon + ")"
-
+        return self.nomRayon
+    
 class Contenir(models.Model):
-    idRayon = models.ForeignKey(Rayon, on_delete=models.CASCADE, related_name="contient")
-    refProd = models.ForeignKey(Produit, on_delete=models.CASCADE, related_name="contient")
-    qte = models.IntegerField()
+    produit = models.ForeignKey(Produit, on_delete=models.CASCADE, related_name="contenir_produit")
+    rayon = models.ForeignKey(Rayon, on_delete=models.CASCADE, related_name="contenir_rayon")
+    Qte = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('produit', 'rayon')  # Un produit dans un rayon spécifique est unique
 
     def __str__(self):
-        return "(" + str(self.idRayon) + "," + str(self.refProd) + "," + str(self.qte) + ")"
+        return f"{self.produit} dans {self.rayon} (Qte: {self.Qte})"
     
