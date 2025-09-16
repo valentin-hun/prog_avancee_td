bab63c5952462221bf969b036903df9cd2c42c69
valentin-hun <valentinhun@hotmail.fr>
2025-09-16 22:31:47 +0200

    filtre produit par status

--- monApp/admin.py
+++ monApp/admin.py
@@ -3,8 +3,38 @@ from .models import Produit, Categorie, Statut, Rayon, Contenir
 
 # Register your models here.
 
+class ProduitFilter(admin.SimpleListFilter):
+    title = 'filtre produit'
+    parameter_name = 'custom_status'
+    def lookups(self, request, model_admin) :
+        return (
+            ('OnLine', 'En ligne'),
+            ('OffLine', 'Hors ligne'),
+        )
+    def queryset(self, request, queryset):
+        if self.value() == 'OnLine':
+            return queryset.filter(status=1)
+        if self.value() == 'OffLine':
+            return queryset.filter(status=0)
+
 class ProduitAdmin(admin.ModelAdmin):
-    list_display = ('intituleProd', 'prixUnitaireProd')
+    model = Produit
+    list_display = ["refProd", "intituleProd", "prixUnitaireProd", "dateFabProd", "categorie", "status"]
+    list_editable = ["intituleProd", "prixUnitaireProd"]
+    radio_fields = {"status": admin.VERTICAL}
+    search_fields = ('intituleProd', 'dateFabProd')
+    list_filter = (ProduitFilter,)
+    date_hierarchy = 'dateFabProd'
+    
+class ProduitInline(admin.TabularInline):
+    model = Produit
+    extra = 1 # nombre de lignes vides par défaut
+
+class CategorieAdmin(admin.ModelAdmin):
+    model = Categorie
+    inlines = [ProduitInline]
+
+
 
 admin.site.register(Produit, ProduitAdmin)
 admin.site.register(Categorie)
