e408dc60d995933478df2f90847d573a4d1bc5f1
valentin-hun <valentin.hun@etu.univ-orleans.fr>
2025-09-18 10:57:28 +0200

    ajout template view fonctionnel home, about_us

--- monApp/views.py
+++ monApp/views.py
@@ -2,15 +2,36 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from monApp.models import *
 from django.contrib.auth.models import User
+from django.views.generic import *
 
-def home(request):
-    if request.GET and request.GET["test"]:
-        raise Http404
-    return HttpResponse("Bonjour Monde!")
 
 def accueil(request,param):
     return HttpResponse("<h1>Hello " + param + " ! You're connected</h1>")
 
+class HomeView(TemplateView):
+    template_name = "monApp/page_home.html"
+    
+    def post(self, request, **kwargs):
+        return render(request, self.template_name)
+    
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['titreh1'] = "Hello DJANGO"
+        if self.kwargs.get('param'):
+            context['titreh1'] = "Hello " + self.kwargs.get('param')
+        return context
+
+class AboutView(TemplateView):
+    template_name = "monApp/about_us.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(AboutView, self).get_context_data(**kwargs)
+        context['titreh1'] = "About us..."
+        return context
+
+    def post(self, request, **kwargs):
+        return render(request, self.template_name)
+    
 # Create your views here.
 
 def ListProduits(request):
@@ -25,7 +46,7 @@ def ListStatuts(request):
     stats = Statut.objects.all()
     return render(request, 'monApp/list_statuts.html',{'stats': stats})
 
-def ListRayon(request):
+def ListRayons(request):
     rayons = Rayon.objects.all()
     return render(request, 'monApp/list_rayons.html',{'rayons': rayons})
 
