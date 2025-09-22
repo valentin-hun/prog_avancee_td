1a9cbf741365994cf7cdbe56f963f20e5c75c4cb
valentin-hun <valentin.hun@etu.univ-orleans.fr>
2025-09-22 09:25:57 +0200

    ajout lien produit

--- monApp/urls.py
+++ monApp/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     path("contact_us/", views.ContactView.as_view(), name="contact_us"),
-    path("home/", views.HomeView.as_view() ,name="home"),
-    path("home/<param>",views.HomeView.as_view() ,name='accueil'),
-    path("about_us/", views.AboutView.as_view() ,name="about_us"),
+    path("home/", views.HomeView.as_view(), name="home"),
+    path("home/<param>",views.HomeView.as_view(), name='accueil'),
+    path("about_us/", views.AboutView.as_view(), name="about_us"),
     path("produits/",views.ProduitListView.as_view()),
-    path("produit/<pk>/",views.ProduitDetailView.as_view()),
-    path('categories/',views.ListCategories ,name='categories'),
-    path('statuts/',views.ListStatuts ,name='statuts'),
-    path('rayons/',views.ListRayons ,name='rayons')
+    path("produit/<pk>/",views.ProduitDetailView.as_view(), name="dtl_prdt"),
+    path('categories/',views.ListCategories, name='categories'),
+    path('statuts/',views.ListStatuts, name='statuts'),
+    path('rayons/',views.ListRayons, name='rayons')
 ]
